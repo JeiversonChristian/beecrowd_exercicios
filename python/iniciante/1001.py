41658d0a250d6e8048db66987ef994b4361edabd
JeiversonChristian <jeiversonc@gmail.com>
2022-07-18 17:27:44 -0300

    1001 em python

---
+++ python/iniciante/1001.py
@@ -0,0 +1,10 @@
+# beecrowd | 1001
+# Extremamente Básico
+# https://www.beecrowd.com.br/judge/pt/problems/view/1001
+
+a = int(input())
+b = int(input())
+x = a + b
+print("X =", x)
+
+# no python é extremamente básico
